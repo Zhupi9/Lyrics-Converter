a22d5cd9981ffc94e17707b388ac5b322c38560d
Zhupi9 <mengxuanping@gmail.com>
2019-10-28 16:14:49 +0800

    second commit

--- source.c
+++ source.c
@@ -11,23 +11,31 @@
 
 #define MAX_LENGTH 2048
 #define MAX_PATH 1024
+#define MAX_NAME 512
 void textProcessor(FILE *lrcx,FILE *lrc);
 int isLineRight(char* buffer);
 void deleteString(char *buffer,int position);
+void getFileName(char* lrcxPath, char* fileName);
+void editPath(char* lrcxPath);
 
 int main(int argc, const char * argv[]) {
     //input the path of .lrcx file
     char lrcxPath[MAX_PATH];
     printf("Drag the .lrcx File:");
-    scanf("%s",lrcxPath);
     
+    scanf("%s",lrcxPath);
+    //edit the path(ep. space,"")
+    char fileName[MAX_NAME];
+    getFileName(lrcxPath, fileName);
+    char outPath[MAX_PATH]="/Users/apple/Desktop/" ;
+    strcat(outPath, fileName);
     //open the .lrcx file by the path above
     FILE *lrcx;
     lrcx=fopen(lrcxPath, "r");
     
     //create a new .lrc file as the output
     FILE *lrc;
-    lrc=fopen("/Users/apple/Desktop/out","w");
+    lrc=fopen(outPath,"w");
     
     /*lyrics text processor*/
     textProcessor(lrcx, lrc);
@@ -74,4 +82,24 @@ void deleteString(char *buffer,int position){
         point++;
     }
     buffer[point]=buffer[point+1];
+}
+
+void editPath(char* lrcxPath){
+    
+}
+
+void getFileName(char* lrcxPath, char* fileName){
+    //find endPoint and the last '/'
+    int endPoint;
+    for(endPoint=0;lrcxPath[endPoint]!='\0';endPoint++){;}
+    int lastSection;
+    for(lastSection=endPoint;lrcxPath[lastSection]!='/';lastSection--){;}
+    lastSection++;
+    int i;
+    //get the file name to the string
+    //endPoint-1 to delete the 'x' out of 'lrcx'
+    for(i=0;lastSection!=endPoint-1;lastSection++,i++){
+        fileName[i]=lrcxPath[lastSection];
+    }
+    
 }
