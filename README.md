454b8f78dd42aa1b6d0eff388b7ac0740cf4c7a9
Zhupi9 <mengxuanping@gmail.com>
2019-10-29 18:06:44 +0800

    second commit

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 A program used to convert .lrcx files to .lrc files which are compliable with SONY NW-ZX300A
 ## First commit
 version 1.0 
-probelems:
+problems:
 1. Can't read file name exactly with " - " (ep. "寂寞列车 - 汪峰.lrcx")
-2. The out put file name is idle as "out"
+2. The out put file name is default as "out"
 3. Can't treat with a Blank Line (with a [00:00:02] at beginning)
+## Second commit
+version 1.2
+fixed all problems in version1.0
+additional:
+1. fixed the dragging problem in macOS
+problems:
+1. output path is default in Desktop(on my own PC)
+2. can't process multiple files simultaneously
--- source.c
+++ source.c
@@ -12,6 +12,7 @@
 #define MAX_LENGTH 2048
 #define MAX_PATH 1024
 #define MAX_NAME 512
+#define true 1
 void textProcessor(FILE *lrcx,FILE *lrc);
 int isLineRight(char* buffer);
 void deleteString(char *buffer,int position);
@@ -19,30 +20,44 @@ void getFileName(char* lrcxPath, char* fileName);
 void editPath(char* lrcxPath);
 
 int main(int argc, const char * argv[]) {
+    
     //input the path of .lrcx file
     char lrcxPath[MAX_PATH];
-    printf("Drag the .lrcx File:");
-    
-    scanf("%s",lrcxPath);
-    //edit the path(ep. space,"")
     char fileName[MAX_NAME];
-    getFileName(lrcxPath, fileName);
     char outPath[MAX_PATH]="/Users/apple/Desktop/" ;
-    strcat(outPath, fileName);
+    while(1){
+        printf("Drag the .lrcx File:");
+        scanf("%[^\n]",lrcxPath); //[^\n] means end only with enter(not space) 
+        setbuf(stdin, NULL);
+     //edit the path(ep. space,"")
+        editPath(lrcxPath);
+     
+        getFileName(lrcxPath, fileName);
+    
+        strcat(outPath, fileName);
     //open the .lrcx file by the path above
-    FILE *lrcx;
-    lrcx=fopen(lrcxPath, "r");
+        FILE *lrcx;
+        lrcx=fopen(lrcxPath, "r");
+    
     
     //create a new .lrc file as the output
-    FILE *lrc;
-    lrc=fopen(outPath,"w");
+        FILE *lrc;
+        lrc=fopen(outPath,"w");
     
     /*lyrics text processor*/
-    textProcessor(lrcx, lrc);
+        textProcessor(lrcx, lrc);
     
     //close the files
-    fclose(lrcx);
-    fclose(lrc);
+        fclose(lrcx);
+        fclose(lrc);
+        memset(lrcxPath, '\0', sizeof(lrcxPath));
+        memset(fileName, '\0', sizeof(fileName));
+        for(int i=0;i<MAX_PATH;i++){
+            if(i>20){
+                outPath[i]='\0';
+            }
+        }
+    }
 }
 
 void textProcessor(FILE *lrcx,FILE *lrc){
@@ -85,7 +100,15 @@ void deleteString(char *buffer,int position){
 }
 
 void editPath(char* lrcxPath){
-    
+    int point;
+    for(point=0;lrcxPath[point]!='\0';point++){
+        if(lrcxPath[point]=='\\'){
+            deleteString(lrcxPath, point);
+        }
+    }
+    if(lrcxPath[point-1]==' '){
+        lrcxPath[point-1]='\0';
+    }
 }
 
 void getFileName(char* lrcxPath, char* fileName){
